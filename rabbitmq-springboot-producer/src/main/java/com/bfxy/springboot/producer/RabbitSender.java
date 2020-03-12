3a90a995a5ab444dc13e49fd51c43866b10a7d10
ant666 <1035079927@qq.com>
2020-03-12 21:49:53 +0800

    整合springboot生产端

--- rabbitmq-springboot-producer/src/main/java/com/bfxy/springboot/producer/RabbitSender.java
+++ rabbitmq-springboot-producer/src/main/java/com/bfxy/springboot/producer/RabbitSender.java
@@ -1,7 +1,6 @@
 package com.bfxy.springboot.producer;
 
-import java.util.Map;
-
+import com.bfxy.springboot.entity.Order;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.core.RabbitTemplate.ConfirmCallback;
 import org.springframework.amqp.rabbit.core.RabbitTemplate.ReturnCallback;
@@ -12,7 +11,7 @@ import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
-import com.bfxy.springboot.entity.Order;
+import java.util.Map;
 
 @Component
 public class RabbitSender {
@@ -22,18 +21,22 @@ public class RabbitSender {
 	private RabbitTemplate rabbitTemplate;  
 	
 	//回调函数: confirm确认
+	//cause异常返回的信息
 	final ConfirmCallback confirmCallback = new RabbitTemplate.ConfirmCallback() {
 		@Override
 		public void confirm(CorrelationData correlationData, boolean ack, String cause) {
 			System.err.println("correlationData: " + correlationData);
 			System.err.println("ack: " + ack);
+			// ack一般去更新数据库，nack则补偿机制，消息百分之百投递方案
 			if(!ack){
+				// TODO 补偿机制
 				System.err.println("异常处理....");
 			}
 		}
 	};
 	
-	//回调函数: return返回
+	//回调函数: return返回，重试机制，重试replyCode错误码，重试replyText错误提示
+	// 如果路由key不存在，则会调用returnedMessage这个方法
 	final ReturnCallback returnCallback = new RabbitTemplate.ReturnCallback() {
 		@Override
 		public void returnedMessage(org.springframework.amqp.core.Message message, int replyCode, String replyText,
@@ -49,7 +52,7 @@ public class RabbitSender {
 		Message msg = MessageBuilder.createMessage(message, mhs);
 		rabbitTemplate.setConfirmCallback(confirmCallback);
 		rabbitTemplate.setReturnCallback(returnCallback);
-		//id + 时间戳 全局唯一 
+		//id + 时间戳 全局唯一 ，这里暂时设置为1234567890
 		CorrelationData correlationData = new CorrelationData("1234567890");
 		rabbitTemplate.convertAndSend("exchange-1", "springboot.abc", msg, correlationData);
 	}
